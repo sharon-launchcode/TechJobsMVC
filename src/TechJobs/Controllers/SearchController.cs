6e4552734d229005e60a29ef354ed7636a76525a
sharon-launchcode <sharon.launchcode@gmail.com>
2018-02-27 16:26:13 -0500

    add ViewBag.columnchoices for Search

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -25,13 +25,11 @@ namespace TechJobs.Controllers
         }
         /// </summary>
 
-        //below necessary this iteration not on List Controller
-        //static private List<string> Searches = new List<string>();
-        //above necessary this iteration not on List Controller
         public IActionResult Index()
         {
-           // ViewBag.columns = ListController.columnChoices;
-            ViewBag.columns = columnChoices;
+            // ViewBag.columns = ListController.columnChoices;
+            ViewBag.columns = SearchController.columnChoices;
+           // ViewBag.columns = columnChoices;
             ViewBag.title = "Search";
             return View();
         }
@@ -39,8 +37,30 @@ namespace TechJobs.Controllers
         // TODO #1 - Create a Results action method to process 
         // search request and display results
 
+
+
+        
+
+        public IActionResult Results(string searchType, string searchTerm)
+            //will enable query parameters with these names searchType searchTerm
+        {
+            //pass data to the view
+
+
+            if (string.IsNullOrEmpty(searchType))
+            {
+                searchType = "SEARCHTYPE NEEDED";
+            }
+            if (string.IsNullOrEmpty(searchTerm))
+            {
+                searchTerm = "SEARCHTERM NEEDED";
+            }
+            return Content(string.Format("searchType is {0} and searchTerm is {1}", searchType, searchTerm), "text/html");
+ 
+        }
+
         /// <summary>
-        /// /
+
         public IActionResult Values(string column)
         {
             if (column.Equals("all"))
@@ -60,34 +80,5 @@ namespace TechJobs.Controllers
             }
         }
         /// </summary>
-
-        
-
-        public IActionResult Results(string searchType, string searchTerm)
-            //will enable query parameters with these names searchType searchTerm
-        {
-            //pass data to the view
-
-
-            if (string.IsNullOrEmpty(searchType))
-            {
-                searchType = "SEARCHTYPE NEEDED";
-            }
-            if (string.IsNullOrEmpty(searchTerm))
-            {
-                searchTerm = "SEARCHTERM NEEDED";
-            }
-            return Content(string.Format("searchType is {0} and searchTerm is {1}", searchType, searchTerm), "text/html");
-           // Searches.Contains(searchType, searchTerm);
-
-            //display the data
-
-
-            //ViewBag.searches = Searches;
-
-            //return View();
-        }
-
-
     }
 }
